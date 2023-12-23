5d0580ebaacaae0438fb3dbfa3b108ed7dbaeb35
Michal Ankiersztajn <aandrobrain@gmail.com>
2023-12-23 15:38:25 +0100

    Add Kotlinx Serialization dependency

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.kotlinSerialization)
 }
 
 kotlin {
@@ -43,7 +44,8 @@ kotlin {
             implementation(compose.ui)
             @OptIn(ExperimentalComposeLibrary::class)
             implementation(compose.components.resources)
-            
+
+            implementation(libs.kotlinx.serialization)
             implementation(libs.decompose)
             implementation(libs.decompose.jetbrains)
         }
